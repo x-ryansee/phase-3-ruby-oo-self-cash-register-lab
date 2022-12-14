dbcb91d651ecc7f62111e93dacff5273df9677cf
Ryan See <ryanjosephsee@gmail.com>
2022-12-14 15:39:35 -0800

    commit tree e69bf8fe445f0ad96c3ba9cd9f31721560f97eac

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
+class CashRegister
+    attr_reader :discount
+    attr_reader :total
 
+    def initialize (discount = 0)
+        @discount = discount
+        @total = 0
+    end
+
+    def add_item (title, price, quantity)
+        self.total += price*quantity
+    end
+
+
+end
+
+
+cash_register_with_discount = CashRegister.new(20)
+cash_register_with_discount.discount
